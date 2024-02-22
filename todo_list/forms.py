2a53c4debba69daa95b801720083b6e4a4e18b87
suryasuresh2429 <122439702+suryasuresh2429@users.noreply.github.com>
2024-02-22 12:37:26 +0530

    Delete todo_list directory

--- todo_list/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import models
-from .models import List
-
-class ListForm(forms.ModelForm):
-    class Meta:
-        model = List
-        fields = ["item", "completed"]
